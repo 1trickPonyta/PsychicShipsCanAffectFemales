513956eaa937e74c7ff8f81d010f36ff4e61495b
1trickPonyta <1trickPonyta@kangaroostandard.com>
2021-07-14 20:21:52 -0700

    Initial commit

---
+++ Source/PsychicShipsCanAffectFemales.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace PsychicShipsCanAffectFemales
+{
+    [StaticConstructorOnStartup]
+    static class PsychicShipsCanAffectFemales
+    {
+        const string PACKAGE_ID = "psychicshipscanaffectfemales.1trickPonyta";
+
+        static PsychicShipsCanAffectFemales()
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+#if DEBUG
+            Log.Message("[Psychic Ships Can Affect Females] Patch applied.");
+#endif
+        }
+    }
+
+    [HarmonyPatch(typeof(CompCauseGameCondition_PsychicEmanation))]
+    [HarmonyPatch("Initialize")]
+    class Patch
+    {
+        static AccessTools.FieldRef<CompCauseGameCondition_PsychicEmanation, Gender> genderRef = 
+            AccessTools.FieldRefAccess<CompCauseGameCondition_PsychicEmanation, Gender>("gender");
+
+        static void Postfix(CompCauseGameCondition_PsychicEmanation __instance)
+        {
+            genderRef(__instance) = Rand.Bool ? Gender.Male : Gender.Female;
+        }
+    }
+}
